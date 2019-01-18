b73a9018954585b19596f8529504885237ac5d51
Read Learn Climb <raphaelkang@gmail.com>
2019-01-17 16:19:48 -0800

    lesson 9

--- app/src/main/java/com/readlearnclimb/swoosh/Controller/FinishActivity.kt
+++ app/src/main/java/com/readlearnclimb/swoosh/Controller/FinishActivity.kt
@@ -2,9 +2,9 @@ package com.readlearnclimb.swoosh.Controller
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import com.readlearnclimb.swoosh.Model.Player
 import com.readlearnclimb.swoosh.R
-import com.readlearnclimb.swoosh.Utilities.EXTRA_LEAGUE
-import com.readlearnclimb.swoosh.Utilities.EXTRA_SKILL
+import com.readlearnclimb.swoosh.Utilities.EXTRA_PLAYER
 import kotlinx.android.synthetic.main.activity_finish.*
 
 class FinishActivity : AppCompatActivity() {
@@ -13,9 +13,8 @@ class FinishActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_finish)
 
-        val league = intent.getStringExtra(EXTRA_LEAGUE)
-        val skill = intent.getStringExtra(EXTRA_SKILL)
+        val player = intent.getParcelableExtra<Player>(EXTRA_PLAYER)
 
-        searchLeaguesText.text = "Looking for $league $skill league near you..."
+        searchLeaguesText.text = "Looking for ${player.league} ${player.skill} league near you..."
     }
 }
--- app/src/main/java/com/readlearnclimb/swoosh/Controller/LeagueActivity.kt
+++ app/src/main/java/com/readlearnclimb/swoosh/Controller/LeagueActivity.kt
@@ -4,13 +4,14 @@ import android.content.Intent
 import android.os.Bundle
 import android.view.View
 import android.widget.Toast
-import com.readlearnclimb.swoosh.Utilities.EXTRA_LEAGUE
+import com.readlearnclimb.swoosh.Model.Player
 import com.readlearnclimb.swoosh.R
+import com.readlearnclimb.swoosh.Utilities.EXTRA_PLAYER
 import kotlinx.android.synthetic.main.activity_league.*
 
 class LeagueActivity : BaseActivity() {
 
-    var selectedLeague = ""
+    var player = Player("", "")
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -21,14 +22,14 @@ class LeagueActivity : BaseActivity() {
         womensLeagueBtn.isChecked = false
         coedLeagueBtn.isChecked = false
 
-        selectedLeague = "mens"
+        player.league = "mens"
     }
 
     fun onWomensClicked(view: View) {
         mensLeagueBtn.isChecked = false
         coedLeagueBtn.isChecked = false
 
-        selectedLeague = "womens"
+        player.league = "womens"
 
     }
 
@@ -36,14 +37,14 @@ class LeagueActivity : BaseActivity() {
         mensLeagueBtn.isChecked = false
         womensLeagueBtn.isChecked = false
 
-        selectedLeague = "co-ed"
+        player.league = "co-ed"
 
     }
 
     fun leagueNextClicked(view: View) {
-        if (selectedLeague != "") {
+        if (player.league != "") {
             val skillActivity = Intent(this, SkillActivity::class.java)
-            skillActivity.putExtra(EXTRA_LEAGUE, selectedLeague)
+            skillActivity.putExtra(EXTRA_PLAYER, player)
             startActivity(skillActivity)
         } else {
             Toast.makeText(this, "Please select a league.", Toast.LENGTH_SHORT).show()
